67b7eb9e948f3383aee8183360bd4e273ce4e38e
Ethan Madison <ethan@ethanmad.com>
2018-10-26 15:13:21 -0400

    BPA: Refactor while loop
    Signed-off-by: Ethan Madison <ethan@ethanmad.com>

--- backend/bravo.py
+++ backend/bravo.py
@@ -36,17 +36,17 @@ for winner in range(w):
         s[winner][loser] = ( V[winner] / (V[winner] + V[loser]) )
 
 # Audit ballots
-m = 0
+m = 0 # number of ballots tested
 reject_count = 0
 num_null_hypotheses = w*l
-while true: # Step 6
+while m < M: # Step 6
     # Step 2: TODO - use seed
     b = randint(0, v) # Pick random ballot ## DUMMY DATA
     # Send 'b' to frontend - TODO API call
     # Recieve list of 'votes' (at most size w) where each element is in [0,n) - TODO API call
     votes = [] # size in [0, w)
     for i in range(w): # TODO: remove this once API calls in place
-        vote = randint(0, n) 
+        votes.append(randint(0, n)) 
     for vote in votes:
         if vote in W: # Step 3
             for loser in L:
@@ -54,15 +54,16 @@ while true: # Step 6
                 if T[vote][loser] >= 1/alpha: # Step 5
                     T[vote][loser] = 0 # Mark T's corresp. null hyp. as rejected
                     reject_count += 1
-        if vote in L: # Step 4
+        elif vote in L: # Step 4
             for winner in W:
                 T[winner][vote] *= (1-s[winner][vote])/.5
                 if T[winner][vote] >= 1/alpha: # Step 5
                     T[winner][vote] = 0 # Mark T's corresp. null hyp. as rejected
                     reject_count += 1
     # Step 6
-    if reject_count >= num_hypotheses:
+    if reject_count >= num_null_hypotheses:
         print("Audit completed: the results stand.")
         break
-    elif m >= M:
-        print("Too many ballots were drawn. Perform a full hand-recount.")
+        
+
+print("Too many ballots were drawn. Perform a full hand-recount.")
