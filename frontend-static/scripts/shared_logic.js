86780fb39b060c343b2a04a45254a0e33dbba90a
Jad Chaar <jad.chaar@gmail.com>
2018-11-01 18:52:49 -0400

    Added front-end and backend logic for BRAVO

--- frontend-static/scripts/shared_logic.js
+++ frontend-static/scripts/shared_logic.js
@@ -1,13 +1,16 @@
+// eslint-disable-next-line
+const API_ROOT = 'http://127.0.0.1:5000'
+
 // Iterate through all navbar anchors and add event listeners
-const navAnchors = document.querySelectorAll("#navbarNav li.nav-item a");
+const navAnchors = document.querySelectorAll('#navbarNav li.nav-item a');
 for (const anchor of navAnchors) {
-    anchor.addEventListener("click", (event) => {
+    anchor.addEventListener('click', (event) => {
         changeActivePage(event);
     });
 }
 
 // Switch Active Page
 function changeActivePage(event) {
-    document.querySelector("#navbarNav li.active").classList.remove("active");
-    event.target.parentNode.classList.add("active");
+    document.querySelector('#navbarNav li.active').classList.remove('active');
+    event.target.parentNode.classList.add('active');
 }
