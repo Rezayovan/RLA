83da188b9676fb8702c7ea5dc95db3d541ca1f3f
Reza <reza@Rezas-MacBook-Pro.local>
2018-12-02 16:39:28 -0500

    basic Cast tests work

--- backend/audits/Cast.py
+++ backend/audits/Cast.py
@@ -1,9 +1,9 @@
 import random
 import math
 import numpy as np
-from .shared_objects.BaseAudit import BaseAudit
-from .shared_objects.Candidates import Candidates
-from .shared_objects.Hypotheses import Hypotheses
+from shared_objects.BaseAudit import BaseAudit
+from shared_objects.Candidates import Candidates
+from shared_objects.Hypotheses import Hypotheses
 
 class Cast(BaseAudit):
 
@@ -19,7 +19,7 @@ class Cast(BaseAudit):
 		self.threshold = threshold
 		self.random_seed = random_seed
 		self.unaudited = np.arange(num_batches)
-		self.alpha = calc_alpha_s(risk_tolerance)
+		self.alpha = self.calc_alpha_s(risk_tolerance)
 		self.reported_batch_info, self.audited_batch_info, self.winners, self.losers = self.init_info()
 
 	def calc_alpha_s(self, risk_tolerance):
@@ -30,34 +30,35 @@ class Cast(BaseAudit):
 
 	def init_info(self):
 		total_votes = np.zeros(self.num_candidates)
-
-		for i in range(self.num_batches):
-			batch_info = get_batch_info()			
+		reported_batch_info = []
+		for _ in range(self.num_batches):
+			batch_info = self.get_batch_info()
 			reported_batch_info.append(batch_info)
 			for idx, num_votes in enumerate(batch_info):
 				total_votes[idx] = total_votes[idx] + num_votes
-	
+
 		losers = np.argsort(total_votes)[:(num_candidates - num_winners)]
 		winners = np.argsort(total_votes)[(num_candidates - num_winners):]
 		reported_batch_info = np.asarray(reported_batch_info)
 		audited_batch_info = np.zeros(reported_batch_info.shape)
-		
+
 		return reported_batch_info, audited_batch_info, winners, losers
 
-	def get_batch_info():
+	def get_batch_info(self):
 		'''
 		Get batch info
 		'''
+		pass
 
 	def calc_adj_margin(self, winner, loser):
 		reported_margin = 0
 		audited_margin = 0
 		for num in self.unaudited:
-			reported_margin = reported_margin + self.reported_batch_info[winner] - self.reported_batch_info[loser]
+			reported_margin = reported_margin + self.reported_batch_info[num][winner] - self.reported_batch_info[num][loser]
 
 		for batches in self.audited_batch_info:
-			audited_margin = audited_margin + self.audited_batch_info[winner] - self.audited_batch_info[loser]
-		
+			audited_margin = audited_margin + batches[winner] - batches[loser]
+
 		return reported_margin + audited_margin
 
 	'''
@@ -74,13 +75,16 @@ class Cast(BaseAudit):
 			for idl, loser in enumerate(self.losers):
 				adj_margins[idw][idl] = self.calc_adj_margin(winner, loser)
 
+		print("adj margins %s" % adj_margins)
+
 		for batch_num in self.unaudited:
 			max_u_p = []
 			for idw, winner in enumerate(self.winners):
 				for idl, loser in enumerate(self.losers):
-					u_p = (self.reported_batch_info[batch_num][winner] - self.reported_batch_info[batch_num][loser] + self.batch_size) / adj_margin[idw][idl]
+					u_p = (self.reported_batch_info[batch_num][winner] - self.reported_batch_info[batch_num][loser] + self.batch_size) / adj_margins[idw][idl]
 					max_u_p.append(u_p)
-			u_ps[batch] = np.amax(max_u_p)
+			u_ps[batch_num] = np.amax(max_u_p)
+		print("u_ups %s" % u_ps)
 		return u_ps
 
 
@@ -98,7 +102,8 @@ class Cast(BaseAudit):
 			else:
 				t_ps.append(threshold)
 				squigglie_u_ps.append(u_p - threshold)
-		T = t_ps.sum()
+		print("tps: %s" % t_ps)
+		T = sum(t_ps)
 		squigglie_u_ps = np.asarray(squigglie_u_ps)
 		return T, squigglie_u_ps
 
@@ -113,9 +118,9 @@ class Cast(BaseAudit):
 		while sum < (1 - T):
 			sum = sum + sorted_squigglie_u_ps[count]
 			count = count + 1
-		
+
 		base = (self.num_unaudited - count) / self.num_unaudited
-		n = math.log(self.alpha, base)
+		n = math.log(base, self.alpha)
 		n = math.ceil(n)
 		return n
 
@@ -139,18 +144,27 @@ class Cast(BaseAudit):
 		random.seed(a = self.random_seed)
 
 		for i in range(self.num_stages):
-			T, squigglie_u_ps = self.calc_T(self)
-			n = self.calc_n()
-			batches_to_audit = random.sample(self.unaudited, n)
+			T, squigglie_u_ps = self.calc_T()
+			n = self.calc_n(T, squigglie_u_ps)
+			batches_to_audit = random.sample(list(self.unaudited), n)
 			for batch_num in batches_to_audit:
 				np.delete(self.unaudited, batch_num)
-				self.audited_batch_info[batch_num] = get_batch_info()
+				self.audited_batch_info[batch_num] = self.get_batch_info()
 			t_s = self.calc_t_s(batches_to_audit)
 			if t_s < self.threshold:
 				print('Audit complete')
 				return
 
 		print('Audit failed full hand recount needed')
-		return 
-		
+		return
 
+# num_candidates, num_winners, num_stages, batch_size
+# num_batches, risk_tolerance, threshold, random_seed
+
+if __name__ == "__main__":
+	num_candidates, num_winners, num_stages, batch_size = (2, 1, 2, 10)
+	num_batches, risk_tolerance, threshold, random_seed = (20, .05, .01, 1234567)
+	params = [num_candidates, num_winners, num_stages, batch_size,
+	num_batches, risk_tolerance, threshold, random_seed]
+	cast = Cast(*params)
+	cast.run_audit()
