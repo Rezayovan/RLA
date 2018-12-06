449994e07faa1499321a3ee6985fc52e53ca1be0
Ethan Madison <ethan@ethanmad.com>
2018-12-06 00:29:50 -0500

    BayesianPolling: Rewrite audit
    - Add symmetric check on equality of projected and reported winners
    - Remove dependency on BaseAudit
    - Add dependency on arrange_candidates for get_winner function
    
    Signed-off-by: Ethan Madison <ethan@ethanmad.com>

--- backend/audits/bayesian_polling.py
+++ backend/audits/bayesian_polling.py
@@ -1,9 +1,9 @@
 # pylint: disable=E0402
 
-from .shared_objects.BaseAudit import BaseAudit
+from .shared_objects.arrange_candidates import get_winners
 from .bptools.bptools import compute_win_probs
 
-class BayesianPolling(BaseAudit):
+class BayesianPolling():
     """ Bayesian ballot-polling audit in Python
     A wrapper class around Ron Rivest's [2018-bptool](https://github.com/ron-rivest/2018-bptool).
     """
@@ -21,20 +21,29 @@ class BayesianPolling(BaseAudit):
         self.risk_limit = risk_limit
         self.seed = seed
 
-    def bayesian_audit(self):
-        reported_winners = self.arrange_candidates().winners
-        bayesian_winners = compute_win_probs(self.sample_tallies, self.num_ballots, self.num_winners, self.seed)
-        for winner in bayesian_winners:
-            if not((winner.i in reported_winners)\
-                    and (winner.p >= 1 - self.risk_limit)):
+    def bayesian_polling_audit(self):
+        """
+        Uses compute_win_probs to verify the election results.
+        Returns true IFF the reported winners and Bayesian projected
+        winners match AND the probability of each projected winner
+        winning is greater than the significance level.
+        """
+        reported_winners = get_winners(self.votes_array, self.num_winners)
+        bayesian_winners = compute_win_probs([self.sample_tallies],\
+                self.num_ballots, self.num_winners, self.seed)
+        reported_set = {w for w in reported_winners}
+        bayesian_set = {w.i for w in bayesian_winners}
+        for projected, reported in zip(bayesian_winners, reported_winners):
+            if not(len(bayesian_winners) == len(reported_winners)\
+                    and (projected.i in reported_set)\
+                    and (reported in bayesian_set)\
+                    and (projected.p >= 1 - self.risk_limit)):
                 return False
         return True
 
     def run_audit(self):
-        audit_result = self.run_audit()
-        self.IS_DONE = True
+        audit_result = self.bayesian_polling_audit()
 
-        print("Audit has been finished")
         if audit_result:
             self.IS_DONE_MESSAGE = "Audit completed: the results stand."
             self.IS_DONE_FLAG = "success"
