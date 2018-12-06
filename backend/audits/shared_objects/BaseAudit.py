2f790afc60658518d442bf7e89a4e3a4dba15847
Ethan Madison <ethan@ethanmad.com>
2018-12-06 00:32:18 -0500

    Fix dependencies
    Signed-off-by: Ethan Madison <ethan@ethanmad.com>

--- backend/audits/shared_objects/BaseAudit.py
+++ backend/audits/shared_objects/BaseAudit.py
@@ -1,15 +1,10 @@
 import threading
 import queue
 
-from Candidates import Candidates
-
 class BaseAudit:
     def __init__(self):
         # Initialized in subclass
         self.num_ballots = None
-        self.num_candidates = None
-        self.votes_array = None
-        self.num_winners = None
 
         # global vars
         self._VOTES_BUFFER = queue.Queue()
@@ -50,18 +45,3 @@ class BaseAudit:
         num_ballots = self.num_ballots
         ballot_to_draw = self.random_gen.randint(1, num_ballots)
         return ballot_to_draw
-
-    def arrange_candidates(self):
-        """
-        From `votes_array`, we can find the winners as the `num_winners` candidates
-        (indices) with the most votes. The losers are the rest of the candidates.
-        """
-        sorted_candidates = sorted(enumerate(self.votes_array), \
-                key=lambda t: t[1], reverse=True)
-
-        # Indices of winners in the votes array
-        winners = set(t[0] for t in sorted_candidates[:self.num_winners])
-        # Indices of losers in the votes array
-        losers = set(t[0] for t in sorted_candidates[self.num_winners:])
-
-        return Candidates(winners, losers)
